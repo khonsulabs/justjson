c154636ea55c92121c29ad8855b6bb7639416860
Jonathan Johnson <jon@khonsulabs.com>
2023-01-21 13:55:22 -0800

    Adding JSONTestSuite files
    Fixes coming in a separate commit.

---
+++ tests/jsontestsuite.rs
@@ -0,0 +1,31 @@
+use std::fs;
+
+use justjson::{parser::ParseConfig, Value};
+
+#[test]
+fn suite() {
+    for entry in fs::read_dir("./tests/JSONTestSuite/").unwrap() {
+        let entry = entry.unwrap();
+        let name = entry.file_name();
+        let Some(name) = name.to_str() else { continue; };
+        let config = ParseConfig::default();
+        if name.ends_with(".json") {
+            println!("Testing {name}");
+            let contents = fs::read(entry.path()).unwrap();
+            let result = Value::from_json_bytes_with_config(&contents, config);
+            match name.chars().next().unwrap() {
+                'i' => {
+                    // Doesn't matter, just don't crash.
+                    println!("i test result: {}", result.is_ok())
+                }
+                'y' => {
+                    result.expect("failed when expecting success");
+                }
+                'n' => {
+                    result.expect_err("success when expecting failure");
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+}
