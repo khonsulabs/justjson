8326b6ed1d531d210a9aee1119e2fcdbcc1245cd
kl-botsu <botsu@khonsulabs.com>
2023-01-23 16:04:55 +0000

    Deploying to gh-pages from @ khonsulabs/justjson@330dbd6e1c0aee775a4d527025dca3a381e4f17e 🚀

--- main/source-files.js
+++ main/source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"justjson":["",[],["cow.rs","doc.rs","error.rs","lib.rs","number.rs","parser.rs","string.rs","value.rs"]]\
+"justjson":["",[],["anystr.rs","doc.rs","error.rs","lib.rs","number.rs","parser.rs","string.rs","value.rs"]]\
 }');
 createSourceSidebar();
