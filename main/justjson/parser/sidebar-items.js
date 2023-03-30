7fb10cdc9e972aa86c511d822655757d50016897
kl-botsu <botsu@khonsulabs.com>
2023-03-30 15:05:58 +0000

    Deploying to gh-pages from @ khonsulabs/justjson@e268537e38d4f8a33a73e03affe1491490faa62e 🚀

--- main/justjson/parser/sidebar-items.js
+++ main/justjson/parser/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["JsonKind","Every type supported by JSON."]],"struct":[["ParseConfig","A JSON Value parsing configuration."],["Parser","Parses JSON, driven by a [`ParseDelegate`]."]],"trait":[["ParseDelegate","A delegate for a [`Parser`]."]]};
+window.SIDEBAR_ITEMS = {"enum":["JsonKind"],"struct":["ParseConfig","Parser"],"trait":["ParseDelegate"]};
